d0c0d7cdb47deb98647f8132c8709233efc07d16
Ines de Palma <ipalmamasaveu@gmail.com>
2021-12-22 20:18:42 +0100

    popUp

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -1,64 +1,63 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { HiOutlineLocationMarker } from "react-icons/hi";
 import moment from "moment";
 import "./ContactCard.scss";
 
 const ContactCard = ({ contacts }) => {
   return (
-    <div className="contact">
+    <div className="ContactCards">
       {contacts &&
         contacts.map((contact, index) => {
           return (
-            <div className="contact__box" key={`${index}-${contact._id}`}>
-              <div className="contact__box-allInfo">
-                <div className="contact__box-personalInfo">
-                  <div>
-                    <span className="contact__box-name">{contact.fullName}</span>
-                  </div>
-                  <div className="contact__box-details">
-                    <div>
-                      <span>{`${contact.contactDirection.address.street}, ${contact.contactDirection.address.directionNumber}, ${contact.contactDirection.address.directionFloor}`}</span>
-                    </div>
-                    <div>
-                      <span>{`${contact.contactDirection.postalCode} ${contact.contactDirection.city}, ${contact.contactDirection.country}`}</span>
-                    </div>
-                    <div>
-                      <span>{contact.company}</span>
-                    </div>
-                  </div>
-                  <div className="contact__box-tags">
-                    {contact.tag.map((tag, index) => {
-                      return (
-                        <span key={`${index}-${tag}`} className="contact__box-tag">
-                          {tag}{" "}
-                        </span>
-                      );
-                    })}
-                  </div>
+            <div className="ContactCard__Card" key={`${index}-${contact._id}`}>
+              
+              <div className="ContactCard__Card--item">
+                <h3>{contact.fullName}</h3>
+                <p><HiOutlineLocationMarker
+                  fontSize="1.2em"
+                  color="#47535B"
+                  style={{marginRight: 5}}
+                /> {` ${contact.contactDirection.address.street}, ${contact.contactDirection.address.directionNumber}, ${contact.contactDirection.address.directionFloor}`}</p>
+                <p>{`${contact.contactDirection.postalCode} ${contact.contactDirection.city}, ${contact.contactDirection.country}`}</p>
+                <p>{contact.company}</p>
+
+                <div className="ContactCard__Card--item__tags">
+                  {contact.tag.map((tag, index) => {
+                    return (
+                      <span
+                        key={`${index}-${tag}`}
+                        className="ContactCard__Card--item__tags__tag"
+                      >
+                        {tag}{" "}
+                      </span>
+                    );
+                  })}
                 </div>
-                <div className="contact__box-contact">
-                  <div>
-                    <span>{contact.contactMobileNumber}</span>
-                    <span> | </span>
-                    <span>{contact.contactPhoneNumber}</span>
-                  </div>
-                  <p>{contact.email}</p>
-                  <div>
-                    <span>0 peticiones</span>
-                  </div>
-                </div>
+              </div>
+
+              <div className="ContactCard__Card--item">
                 <div>
-                  <span>Comentarios</span>
-                  <div className="contact__box-comments">
-                    <textarea defaultValue={contact.contactComments}></textarea>
-                  </div>
+                  <span>{contact.contactMobileNumber}</span>
+                  <span> | </span>
+                  <span>{contact.contactPhoneNumber}</span>
                 </div>
-                <div className="contact__box-end">
-                  <span>Creado el {moment(contact.createdAt).format("L")}</span>
-                  <NavLink to={`/contacts/${contact._id}`}>
-                    <button>Abrir Ficha</button>
-                  </NavLink>
+                <p>{contact.email}</p>
+                <div>
+                  <span>0 peticiones</span>
                 </div>
+              </div>
+
+              <div className="ContactCard__Card--item">
+                <span>Comentarios</span>
+                <textarea defaultValue={contact.contactComments}></textarea>
+              </div>
+
+              <div className="ContactCard__Card--item">
+                <span>Creado el {moment(contact.createdAt).format("L")}</span>
+                <NavLink to={`/contacts/${contact._id}`}>
+                  <button>Abrir Ficha</button>
+                </NavLink>
               </div>
             </div>
           );
