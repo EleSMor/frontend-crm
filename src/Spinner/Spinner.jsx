9648c4f583006bac4686d5bb629a2d695230b59d
Ines de Palma <ipalmamasaveu@gmail.com>
2021-12-23 23:52:45 +0100

    some

--- src/Spinner/Spinner.jsx
+++ src/Spinner/Spinner.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
 import BounceLoader from "react-spinners/BounceLoader";
-import "./Spinner.scss"
+import "./Spinner.scss";
 
-const Spinner = () => {
-  return (
-    <div className="Spinner"><BounceLoader size='55' color="#2B363D" /></div>
+const Spinner = () => (
+  <div className="Spinner">
+    <BounceLoader size="55" color="#2B363D" />
+  </div>
+);
 
-  )
-}
-
-export default Spinner
+export default Spinner;
