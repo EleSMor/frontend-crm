558abefa6107b4df2f358670a12d3c6c2be57e00
Ines de Palma <ipalmamasaveu@gmail.com>
2022-01-09 13:54:41 +0100

    petitions

--- src/components/ContactRequestCard/ContactRequestCard.jsx
+++ src/components/ContactRequestCard/ContactRequestCard.jsx
@@ -8,6 +8,7 @@ import { BsChevronDoubleDown, BsChevronDoubleUp } from "react-icons/bs";
 import moment from "moment";
 import "moment/locale/es";
 import "./ContactRequestCard.scss";
+import useWindowSize from "../../hooks/useWindowSize";
 
 const ContactRequestCard = ({ request }) => {
   const formatCurrency = (value) => {
@@ -16,6 +17,7 @@ const ContactRequestCard = ({ request }) => {
       currency: "EUR",
     });
   };
+  const size = useWindowSize();
 
   const maskValues = (value, ref) => {
     let render = "";
@@ -45,90 +47,104 @@ const ContactRequestCard = ({ request }) => {
       key={`${request._id}-${request.requestReference}`}
       className="RequestCard"
     >
-      <div className="RequestCard__Card">
-        <div className="RequestCard__Card--content">
-          <div className="RequestCard__Card--content-header">
-            <h4>Petición {`#${request.requestReference}`}</h4>
-            <p>
-              <BsPersonCircle
-                fontSize="1.4em"
-                color="#47535B"
-                style={{ margin: "0 8 0 48" }}
-              />
-              <span>
-                {request.requestConsultant.fullName
-                  ? request.requestConsultant.fullName
-                  : "Sin consultor"}
-              </span>
-            </p>
-
-            {request.requestBuildingType.map((buildingType, index) => {
-              return (
-                <p
-                  key={`${index}-${request.buildingType}`}
-                  className="RequestCard__Card--content-header"
-                  style={{ marginLeft: 24 }}
-                >
-                  <GoPrimitiveDot
-                    fontSize="1.1em"
-                    color="#47535B"
-                    style={{ marginRight: 4, fontWeight: 2000 }}
-                  />
-                  {buildingType}
-                </p>
-              );
-            })}
+      {size < 880 ? (
+        <div className="RequestCard__CardMobile">
+          <div className="RequestCard__CardMobile--header">
+            <div className="RequestCard__CardMobile--header-title">
+              <h4>Petición {`#${request.requestReference}`}</h4>
+              <p>
+                <BsPersonCircle
+                  fontSize="1.4em"
+                  color="#47535B"
+                  style={{ margin: "0 8 0 0" }}
+                />
+                <span>
+                  {request.requestConsultant.fullName
+                    ? request.requestConsultant.fullName
+                    : "Sin consultor"}
+                </span>
+              </p>
+            </div>
+            <span>Creado el {moment(request.createdAt).format("L")}</span>
+            {request.requestBuildingType.length > 0 && (
+              <div className="RequestCard__CardMobile--header-zones">
+                {request.requestBuildingType.map((buildingType, index) => {
+                  return (
+                    <p key={`${index}-${request.buildingType}`} className="">
+                      <GoPrimitiveDot
+                        fontSize="1.1em"
+                        color="#47535B"
+                        style={{ marginRight: 4, fontWeight: 2000 }}
+                      />
+                      {buildingType}
+                    </p>
+                  );
+                })}
+              </div>
+            )}
           </div>
 
-          <div className="RequestCard__Card--content-body">
-            <div className="RequestCard__Card--content-body--item">
-              <h4 className="RequestCard__Card--content-body--title">
-                <GiPriceTag fontSize="1.2em" style={{ marginRight: 7 }} />
+          <div className="RequestCard__CardMobile--body">
+            <div className="RequestCard__CardMobile--body--item">
+              <h4 className="RequestCard__CardMobile--body--item--title">
+                <GiPriceTag fontSize="1em" style={{ marginRight: 7 }} />
                 <b>Precio</b>
               </h4>
-              <div className="RequestCard__Card--content-body--content">
+              <div className="RequestCard__CardMobile--body--item--content">
                 <div>
                   <BsChevronDoubleUp
-                    fontSize="0.8em"
-                    style={{ marginRight: 7 }}
+                    fontSize="0.9em"
+                    style={{ marginRight: 9 }}
                   />
                   {maskValues(request.requestSalePrice.salePriceMax, "price")}
                 </div>
                 <div>
                   <BsChevronDoubleDown
-                    fontSize="0.8em"
-                    style={{ marginRight: 7 }}
+                    fontSize="0.9em"
+                    style={{ marginRight: 9, marginLeft: 18 }}
                   />
                   {maskValues(request.requestSalePrice.salePriceMin, "price")}
                 </div>
               </div>
             </div>
-            <div className="RequestCard__Card--content-body--item">
-              <h4 className="RequestCard__Card--content-body--title">
-                <GiPapers fontSize="1.2em" style={{ marginRight: 7 }} />
+            <div className="RequestCard__CardMobile--body--item">
+              <h4 className="RequestCard__CardMobile--body--item--title">
+                <GiPapers fontSize="1em" style={{ marginRight: 7 }} />
                 <b>Superficie construida</b>
               </h4>
-              <div>
-                {maskValues(
-                  request.requestBuildSurface.buildSurfaceMax,
-                  "buildSurface"
-                )}
-                {maskValues(
-                  request.requestBuildSurface.buildSurfaceMin,
-                  "buildSurface"
-                )}
-              </div>
-            </div>
-            <div className="RequestCard__Card--content-body--item">
-              <h4 className="RequestCard__Card--content-body--title">
-                <IoIosResize fontSize="1.2em" style={{ marginRight: 7 }} />
-                <b>Superficie de parcela</b>
-              </h4>
-              <div className="RequestCard__Card--content-body--content">
+              <div className="RequestCard__CardMobile--body--item--content">
                 <div>
                   <BsChevronDoubleUp
-                    fontSize="0.8em"
-                    style={{ marginRight: 7 }}
+                    fontSize="0.9em"
+                    style={{ marginRight: 9 }}
+                  />
+                  {maskValues(
+                    request.requestBuildSurface.buildSurfaceMax,
+                    "buildSurface"
+                  )}
+                </div>
+                <div>
+                  <BsChevronDoubleDown
+                    fontSize="0.9em"
+                    style={{ marginRight: 9, marginLeft: 18 }}
+                  />
+                  {maskValues(
+                    request.requestBuildSurface.buildSurfaceMin,
+                    "buildSurface"
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="RequestCard__CardMobile--body--item">
+              <h4 className="RequestCard__CardMobile--body--item--title">
+                <IoIosResize fontSize="1em" style={{ marginRight: 7 }} />
+                <b>Superficie de parcela</b>
+              </h4>
+              <div className="RequestCard__CardMobile--body--item--content">
+                <div>
+                  <BsChevronDoubleUp
+                    fontSize="0.9em"
+                    style={{ marginRight: 9 }}
                   />
                   {maskValues(
                     request.requestPlotSurface.plotSurfaceMax,
@@ -138,7 +154,7 @@ const ContactRequestCard = ({ request }) => {
                 <div>
                   <BsChevronDoubleDown
                     fontSize="0.8em"
-                    style={{ marginRight: 7 }}
+                    style={{ marginRight: 9, marginLeft: 18 }}
                   />
                   {maskValues(
                     request.requestPlotSurface.plotSurfaceMin,
@@ -148,17 +164,137 @@ const ContactRequestCard = ({ request }) => {
               </div>
             </div>
           </div>
+          <div className="RequestCard__CardMobile--body--item--cta">
+            <Link
+              className="RequestCard__CardMobile--body--item--cta--button"
+              to={`/peticiones/${request._id}`}
+            >
+              Consultar
+            </Link>
+          </div>
         </div>
-        <div className="RequestCard__Card--item RequestCard__Card--item-cta">
-          <span>Creado el {moment(request.createdAt).format("L")}</span>
-          <Link
-            className="RequestCard__Card--item-cta--button"
-            to={`/peticiones/${request._id}`}
-          >
-            Consultar
-          </Link>
+      ) : (
+        <div className="RequestCard__Card">
+          <div className="RequestCard__Card--content">
+            <div className="RequestCard__Card--content-header">
+              <h4>Petición {`#${request.requestReference}`}</h4>
+              <p>
+                <BsPersonCircle
+                  fontSize="1.4em"
+                  color="#47535B"
+                  style={{ margin: "0 8 0 48" }}
+                />
+                <span>
+                  {request.requestConsultant.fullName
+                    ? request.requestConsultant.fullName
+                    : "Sin consultor"}
+                </span>
+              </p>
+
+              {request.requestBuildingType.map((buildingType, index) => {
+                return (
+                  <p
+                    key={`${index}-${request.buildingType}`}
+                    className="RequestCard__Card--content-header"
+                    style={{ marginLeft: 24 }}
+                  >
+                    <GoPrimitiveDot
+                      fontSize="1.1em"
+                      color="#47535B"
+                      style={{ marginRight: 4, fontWeight: 2000 }}
+                    />
+                    {buildingType}
+                  </p>
+                );
+              })}
+            </div>
+
+            <div className="RequestCard__Card--content-body">
+              <div className="RequestCard__Card--content-body--item">
+                <h4 className="RequestCard__Card--content-body--title">
+                  <GiPriceTag fontSize="1.2em" style={{ marginRight: 7 }} />
+                  <b>Precio</b>
+                </h4>
+                <div className="RequestCard__Card--content-body--content">
+                  <div>
+                    <BsChevronDoubleUp
+                      fontSize="0.8em"
+                      style={{ marginRight: 7 }}
+                    />
+                    {maskValues(request.requestSalePrice.salePriceMax, "price")}
+                  </div>
+                  <div>
+                    <BsChevronDoubleDown
+                      fontSize="0.8em"
+                      style={{ marginRight: 7 }}
+                    />
+                    {maskValues(request.requestSalePrice.salePriceMin, "price")}
+                  </div>
+                </div>
+              </div>
+              <div className="RequestCard__Card--content-body--item">
+                <h4 className="RequestCard__Card--content-body--title">
+                  <GiPapers fontSize="1.2em" style={{ marginRight: 7 }} />
+                  <b>Superficie construida</b>
+                </h4>
+                <div className="RequestCard__Card--content-body--content">
+                  <div>
+                    <BsChevronDoubleUp
+                      fontSize="0.8em"
+                      style={{ marginRight: 7 }}
+                    />
+                    {maskValues(request.requestBuildSurface.buildSurfaceMax, "price")}
+                  </div>
+                  <div>
+                    <BsChevronDoubleDown
+                      fontSize="0.8em"
+                      style={{ marginRight: 7 }}
+                    />
+                    {maskValues(request.requestBuildSurface.buildSurfaceMin, "price")}
+                  </div>
+                </div>
+              </div>
+              <div className="RequestCard__Card--content-body--item">
+                <h4 className="RequestCard__Card--content-body--title">
+                  <IoIosResize fontSize="1.2em" style={{ marginRight: 7 }} />
+                  <b>Superficie de parcela</b>
+                </h4>
+                <div className="RequestCard__Card--content-body--content">
+                  <div>
+                    <BsChevronDoubleUp
+                      fontSize="0.8em"
+                      style={{ marginRight: 7 }}
+                    />
+                    {maskValues(
+                      request.requestPlotSurface.plotSurfaceMax,
+                      "plotSurface"
+                    )}
+                  </div>
+                  <div>
+                    <BsChevronDoubleDown
+                      fontSize="0.8em"
+                      style={{ marginRight: 7 }}
+                    />
+                    {maskValues(
+                      request.requestPlotSurface.plotSurfaceMin,
+                      "plotSurface"
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="RequestCard__Card--item RequestCard__Card--item-cta">
+            <span>Creado el {moment(request.createdAt).format("L")}</span>
+            <Link
+              className="RequestCard__Card--item-cta--button"
+              to={`/peticiones/${request._id}`}
+            >
+              Consultar
+            </Link>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
